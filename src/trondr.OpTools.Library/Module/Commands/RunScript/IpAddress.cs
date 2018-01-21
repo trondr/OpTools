ccadcd87a80869254c1b01deaa9d456dcb7dc0b7
trondr <trondr@outlook.com>
2018-01-21 12:12:13 +0100

    Add IpAddress class

---
+++ src/trondr.OpTools.Library/Module/Commands/RunScript/IpAddress.cs
@@ -0,0 +1,22 @@
+using System;
+using LanguageExt;
+
+namespace trondr.OpTools.Library.Module.Commands.RunScript
+{
+    public class IpAddress
+    {
+        private IpAddress(string ipAddress)
+        {
+            Value = ipAddress;
+        }
+
+        public string Value { get; }
+
+        public static Result<IpAddress> Create(string ipAdress)
+        {
+            if (F.IsValidIpAddress(ipAdress))
+                return new Result<IpAddress>(new IpAddress(ipAdress));
+            return new Result<IpAddress>(new InvalidOperationException($"'{ipAdress}' is not a valid ip address."));
+        }
+    }
+}
