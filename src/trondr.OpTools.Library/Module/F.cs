8d530d00aaefa93208e41e1819907d27c5eb98b9
trondr <trondr@outlook.com>
2018-01-21 21:55:21 +0100

    Implement GetOnlineHostsToProcess function

--- src/trondr.OpTools.Library/Module/F.cs
+++ src/trondr.OpTools.Library/Module/F.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using trondr.OpTools.Library.Module.Commands.RunScript;
 
 namespace trondr.OpTools.Library.Module
 {
@@ -21,6 +20,35 @@ namespace trondr.OpTools.Library.Module
         public static bool IsValidIpAddress(string ipAdress)
         {
             return ValidIpAddressRegex.IsMatch(ipAdress);
+        }
+
+        public static IEnumerable<Host> GetOnlineHostsToProcess(Dictionary<Hostname, Host> hosts, int samplePercent)
+        {
+            var onlineHosts = hosts
+                .Where(pair => pair.Value.OnlineStatus == OnlineStatus.Online)
+                .Select(pair => pair.Value);
+            if(samplePercent == 100)
+                return onlineHosts;
+            return SampleHosts(onlineHosts, samplePercent);
+        }
+
+        private static IEnumerable<Host> SampleHosts(IEnumerable<Host> onlineHosts, int samplePercent)
+        {
+            var onLineHostsArray = onlineHosts.ToArray();
+            var hostsToReturnCount = onLineHostsArray.Length * samplePercent / 100;
+            var random = new Random();
+            var returnedHostsCount = 0;
+            var allreadyReturned = new Dictionary<int,int>();
+            while (returnedHostsCount < hostsToReturnCount)
+            {
+                var indexToReturn = random.Next(0, onLineHostsArray.Length);
+                if(!allreadyReturned.ContainsKey(indexToReturn))
+                {
+                    allreadyReturned.Add(indexToReturn,indexToReturn);
+                    yield return onLineHostsArray[indexToReturn];
+                    returnedHostsCount++;
+                }
+            }            
         }
     }
 }
--- src/trondr.OpTools.Tests/UnitTests/FTests.cs
+++ src/trondr.OpTools.Tests/UnitTests/FTests.cs
@@ -1,5 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using NUnit.Framework;
 using trondr.OpTools.Library.Module;
+using trondr.OpTools.Library.Module.Commands.RunScript;
 
 namespace trondr.OpTools.Tests.UnitTests
 {
@@ -17,7 +21,6 @@ namespace trondr.OpTools.Tests.UnitTests
             Assert.AreEqual(expected, actual, $"{hostName}");
         }
 
-
         [Test]
         [TestCase("hostname", false)]
         [TestCase("1hostname", false)]
@@ -28,5 +31,158 @@ namespace trondr.OpTools.Tests.UnitTests
             var actual = F.IsValidIpAddress(ipAddress);
             Assert.AreEqual(expected, actual, $"{ipAddress}");
         }
+
+        [Test]        
+        public void GetOnlineHostsToProcessTest()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();           
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 100).ToList();
+            Assert.AreEqual(expected:8,actual:actual.Count,message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample20Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 20).ToList();
+            Assert.AreEqual(expected: 1, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample30Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 30).ToList();
+            Assert.AreEqual(expected: 2, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample40Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 40).ToList();
+            Assert.AreEqual(expected: 3, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample50Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 50).ToList();
+            Assert.AreEqual(expected: 4, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample55Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 55).ToList();
+            Assert.AreEqual(expected: 4, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample60Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 60).ToList();
+            Assert.AreEqual(expected: 4, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample70Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 70).ToList();
+            Assert.AreEqual(expected: 5, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample80Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 80).ToList();
+            Assert.AreEqual(expected: 6, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample90Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 90).ToList();
+            Assert.AreEqual(expected: 7, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample95Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 95).ToList();
+            Assert.AreEqual(expected: 7, actual: actual.Count, message: "Number of hosts");
+        }
+
+        [Test]
+        public void GetOnlineHostsToProcessSample99Test()
+        {
+            var testHosts = GetTestHosts8of10IsOnline();
+            var actual = F.GetOnlineHostsToProcess(hosts: testHosts, samplePercent: 99).ToList();
+            Assert.AreEqual(expected: 7, actual: actual.Count, message: "Number of hosts");
+        }
+
+        private Dictionary<Hostname, Host> GetTestHosts8of10IsOnline()
+        {
+            var hostName01 = GetHostName("computer01");
+            var hostName02 = GetHostName("computer02");
+            var hostName03 = GetHostName("computer03");
+            var hostName04 = GetHostName("computer04");
+            var hostName05 = GetHostName("computer05");
+            var hostName06 = GetHostName("computer06");
+            var hostName07 = GetHostName("computer07");
+            var hostName08 = GetHostName("computer08");
+            var hostName09 = GetHostName("computer09");
+            var hostName10 = GetHostName("computer10");
+
+            var ipAddress01 = GetIpAddress("192.168.1.101");
+            var ipAddress02 = GetIpAddress("192.168.1.102");
+            var ipAddress03 = GetIpAddress("192.168.1.103");
+            var ipAddress04 = GetIpAddress("192.168.1.104");
+            var ipAddress05 = GetIpAddress("192.168.1.105");
+            var ipAddress06 = GetIpAddress("192.168.1.106");
+            var ipAddress07 = GetIpAddress("192.168.1.107");
+            var ipAddress08 = GetIpAddress("192.168.1.108");
+            var ipAddress09 = GetIpAddress("192.168.1.109");
+            var ipAddress10 = GetIpAddress("192.168.1.110");
+
+            var testHosts = new Dictionary<Hostname, Host>()
+            {
+                {hostName01, new Host(hostName01, ipAddress01, OnlineStatus.Online)},
+                {hostName02, new Host(hostName02, ipAddress02, OnlineStatus.Offline)},
+                {hostName03, new Host(hostName03, ipAddress03, OnlineStatus.Online)},
+                {hostName04, new Host(hostName04, ipAddress04, OnlineStatus.Online)},
+                {hostName05, new Host(hostName05, ipAddress05, OnlineStatus.Online)},
+                {hostName06, new Host(hostName06, ipAddress06, OnlineStatus.Online)},
+                {hostName07, new Host(hostName07, ipAddress07, OnlineStatus.Online)},
+                {hostName08, new Host(hostName08, ipAddress08, OnlineStatus.Online)},
+                {hostName09, new Host(hostName09, ipAddress09, OnlineStatus.Offline)},
+                {hostName10, new Host(hostName10, ipAddress10, OnlineStatus.Online)}
+            };
+
+            return testHosts;
+        }
+
+
+        Hostname GetHostName(string hostNameString)
+        {
+            Hostname hostName = null;
+            Hostname.Create(hostNameString).IfSucc(hostname => hostName = hostname);
+            return hostName;
+        }
+
+        IpAddress GetIpAddress(string ipAddressString)
+        {
+            IpAddress ipAddress = null;
+            IpAddress.Create(ipAddressString).IfSucc(address => ipAddress = address);
+            return ipAddress;
+        }
+
     }
 }
