7f37be577234e73f7e99d8bef428d4a9e49d2156
trondr <trondr@outlook.com>
2018-01-23 17:43:14 +0100

    Code cleanup

--- src/trondr.OpTools.Library/Module/Commands/RunScript/Host.cs
+++ src/trondr.OpTools.Library/Module/Commands/RunScript/Host.cs
@@ -4,13 +4,13 @@ namespace trondr.OpTools.Library.Module.Commands.RunScript
 {
     public class Host: Record<Host>
     {
-        public Hostname HostName { get; }
+        public HostName HostName { get; }
         [OptOutOfHashCode]
         public IpAddress IpAddress { get; }
         [OptOutOfHashCode]
         public OnlineStatus OnlineStatus { get;}
 
-        public Host(Hostname hostName, IpAddress ipAddress, OnlineStatus onlineStatus)
+        public Host(HostName hostName, IpAddress ipAddress, OnlineStatus onlineStatus)
         {
             HostName = hostName;
             IpAddress = ipAddress;
--- src/trondr.OpTools.Library/Module/Commands/RunScript/Hostname.cs
+++ src/trondr.OpTools.Library/Module/Commands/RunScript/Hostname.cs
@@ -3,20 +3,20 @@ using LanguageExt;
 
 namespace trondr.OpTools.Library.Module.Commands.RunScript
 {
-    public class Hostname : Record<Hostname>
+    public class HostName : Record<HostName>
     {
         public string Value { get; }
 
-        private Hostname(string hostName)
+        private HostName(string hostName)
         {
             Value = hostName;
         }
 
-        public static Result<Hostname> Create(string hostName)
+        public static Result<HostName> Create(string hostName)
         {
             if (F.IsValidHostName(hostName))
-                return new Result<Hostname>(new Hostname(hostName));
-            return new Result<Hostname>(new InvalidOperationException($"'{hostName}' is not a valid host name."));
+                return new Result<HostName>(new HostName(hostName));
+            return new Result<HostName>(new InvalidOperationException($"'{hostName}' is not a valid host name."));
         }        
     }
 }
