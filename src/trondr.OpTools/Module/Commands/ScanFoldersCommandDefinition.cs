cb7a607621a717b8b488408a0c148c3816510191
trondr <trondr@outlook.com>
2018-02-03 15:32:34 +0100

    Add support for running ScanFolders command with SeBackupPrivilege

--- src/trondr.OpTools/Module/Commands/ScanFoldersCommandDefinition.cs
+++ src/trondr.OpTools/Module/Commands/ScanFoldersCommandDefinition.cs
@@ -1,10 +1,14 @@
-﻿using Akka.Actor;
+﻿using System;
+using System.Diagnostics;
+using Akka.Actor;
 using Akka.DI.CastleWindsor;
 using Akka.DI.Core;
 using Castle.Windsor;
+using Common.Logging;
 using NCmdLiner.Attributes;
 using trondr.OpTools.Library.Infrastructure;
 using trondr.OpTools.Library.Module.Commands.ScanFolders;
+using trondr.OpTools.Library.Module.Common.Security;
 
 namespace trondr.OpTools.Module.Commands
 {
@@ -12,11 +16,13 @@ namespace trondr.OpTools.Module.Commands
     {
         private readonly IScanFoldersCommandProviderFactory _scanFoldersCommandProviderFactory;
         private readonly IWindsorContainer _windsorContainer;
+        private readonly ILog _logger;
 
-        public ScanFoldersCommandDefinition(IScanFoldersCommandProviderFactory scanFoldersCommandProviderFactory, IWindsorContainer windsorContainer)
+        public ScanFoldersCommandDefinition(IScanFoldersCommandProviderFactory scanFoldersCommandProviderFactory, IWindsorContainer windsorContainer, ILog logger)
         {
             _scanFoldersCommandProviderFactory = scanFoldersCommandProviderFactory;
             _windsorContainer = windsorContainer;
+            _logger = logger;
         }
 
         [Command(Description = "Scan folder usage and folder security. Result is output to Folder-Usage-<hostname>.csv and Folder-Security-<hostname>.csv. Hostname is derived from the unc paths.")]
@@ -26,10 +32,36 @@ namespace trondr.OpTools.Module.Commands
             [RequiredCommandParameter(Description = "The result will be written to the data folder during scan.", AlternativeName = "df", ExampleValue = @"c:\temp\data")]
             string localDataFolder,
             [RequiredCommandParameter(Description = "The result will be uploaded to this folder when scan is done.", AlternativeName = "uf", ExampleValue = @"\\dataserver1\data\folderreports")]
-            string uploadDataFolder
+            string uploadDataFolder,
+            [OptionalCommandParameter(Description = "Run folder scan with SeBackupPrivilege to ignore any access control entries on folders and files blocking the way.", DefaultValue = false,ExampleValue = false,AlternativeName = "rbp")]
+            bool runWithBackupPrivilege
             )
         {
+            int exitCode;
+            if (runWithBackupPrivilege)
+            {
+                using (new AdjustPrivilege(PrivilegeName.SeBackupPrivilege))
+                {
+                    if (!PrivilegeProvider.HasPrivilege(Environment.MachineName, Process.GetCurrentProcess(), PrivilegeName.SeBackupPrivilege))
+                    {
+                        _logger.Error($"Failed to enable privilege: '{PrivilegeName.SeBackupPrivilege}'");
+                        exitCode = 1;
+                    }
+                    else
+                    {
+                        exitCode = RunScanFolders(uncPathsToScan, localDataFolder, uploadDataFolder);
+                    }                    
+                }
+            }
+            else
+            {
+                exitCode = RunScanFolders(uncPathsToScan, localDataFolder, uploadDataFolder);
+            }
+            return exitCode;
+        }
 
+        private int RunScanFolders(string[] uncPathsToScan, string localDataFolder, string uploadDataFolder)
+        {
             using (var scanFoldersActorSystem = ActorSystem.Create("ScanFoldersActorSystem"))
             {
                 var windsorDependencyResolver = new WindsorDependencyResolver(_windsorContainer, scanFoldersActorSystem);
