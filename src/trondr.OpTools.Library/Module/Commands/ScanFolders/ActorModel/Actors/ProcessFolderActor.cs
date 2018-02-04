3311cd42c31ebb13b8031bf1770d95944fbd8086
trondr <trondr@outlook.com>
2018-02-04 01:40:12 +0100

    Started implementing ProcessFolderActor

---
+++ src/trondr.OpTools.Library/Module/Commands/ScanFolders/ActorModel/Actors/ProcessFolderActor.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Akka.Actor;
+using Akka.Util.Internal;
+using trondr.OpTools.Library.Infrastructure;
+using trondr.OpTools.Library.Module.Commands.ScanFolders.ActorModel.Messages;
+using DirectoryInfo = Pri.LongPath.DirectoryInfo;
+
+namespace trondr.OpTools.Library.Module.Commands.ScanFolders.ActorModel.Actors
+{
+    public class ProcessFolderActor : ReceiveActor
+    {
+        public ProcessFolderActor()
+        {
+            Receive<ProcessFolderMessage>(message => OnProcessFolderMessage(message));
+        }
+
+        private void OnProcessFolderMessage(ProcessFolderMessage message)
+        {
+            var directory = new DirectoryInfo(message.UncPath);
+
+            var subDirectories = directory.GetDirectories();
+            subDirectories.ForEach(info => Context.Parent.Tell(new ProcessFolderMessage(message.HostName,info.FullName,message.UsageWriterActor)));
+
+            var files = directory.GetFiles();
+            var size = files.Sum(file => file.Length);
+            ToDo.Implement(ToDoPriority.Critical,"trondr","Implement get of sddl");
+            var sddl = "";
+            var comment = ""; //Any error message here, otherwise empty
+            message.UsageWriterActor.Tell(new UsageRecordMessage(message.HostName, size.ToString(), directory.FullName, sddl,comment));
+        }
+    }
+}
---
+++ src/trondr.OpTools.Library/Module/Commands/ScanFolders/ActorModel/Messages/ProcessFolderMessage.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+
+namespace trondr.OpTools.Library.Module.Commands.ScanFolders.ActorModel.Messages
+{
+    public class ProcessFolderMessage
+    {
+        public ProcessFolderMessage(string hostName, string uncPath, IActorRef usageWriterActor)
+        {
+            UncPath = uncPath;
+            UsageWriterActor = usageWriterActor;
+            HostName = hostName;
+        }
+
+        public string HostName { get; }
+        public string UncPath { get; }
+        public IActorRef UsageWriterActor { get; }
+        
+    }
+}
